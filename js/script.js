d86b345806926ee57e0b90fbaa0c93e1c5dee84d
igbokwe-kosi <igbokwekosiso@gmail.com>
2022-02-03 22:11:52 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+const headerEL = document.querySelector(".header");
+const btnOpenEL = document.querySelector(".btn--open");
+const btnCloseEL = document.querySelector(".btn--close");
+
+btnOpenEL.addEventListener("click", function () {
+  headerEL.classList.add("nav-open");
+});
+btnCloseEL.addEventListener("click", function () {
+  headerEL.classList.remove("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".date");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
