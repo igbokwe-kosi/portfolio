26c2837806a9159cd16fdf0ed5eb77cab2dc6154
igbokwe-kosi <igbokwekosiso@gmail.com>
2022-05-05 08:45:42 +0100

    Added new changes

--- js/script.js
+++ js/script.js
@@ -1,56 +1,51 @@
-const headerEL = document.querySelector(".header");
-const btnOpenEL = document.querySelector(".btn--open");
-const btnCloseEL = document.querySelector(".btn--close");
+const headerEL = document.querySelector('.header');
+const btnToggle = document.querySelector('.btn--toggle');
 
-window.addEventListener('load', ()=>{
-  alert('Website still in the works but you can check it out though!')
-})
+// window.addEventListener('load', () => {
+//   alert('Website still in the works but you can check it out though!');
+// }, );
 
-btnOpenEL.addEventListener("click", function () {
-  headerEL.classList.add("nav-open");
-});
-btnCloseEL.addEventListener("click", function () {
-  headerEL.classList.remove("nav-open");
+btnToggle.addEventListener('click', function () {
+  btnToggle.classList.toggle('btn--close');
+  headerEL.classList.toggle('nav-open');
 });
 
 ///////////////////////////////////////////////////////////
 // Set current year
-const yearEl = document.querySelector(".date");
+const yearEl = document.querySelector('.date');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
-const allLinks = document.querySelectorAll("a:link");
+const allLinks = document.querySelectorAll('a:link');
 
 allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
+  link.addEventListener('click', function (e) {
     e.preventDefault();
-    headerEL.classList.remove("nav-open");
-    const href = link.getAttribute("href");
+    headerEL.classList.remove('nav-open');
+    const href = link.getAttribute('href');
 
     // Scroll back to top
-    if (href === "#")
+    if (href === '#')
       window.scrollTo({
         top: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
 
     // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
+    if (href !== '#' && href.startsWith('#')) {
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
     }
-
-
   });
 });
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 
-const sectionHeroEl = document.querySelector(".section-hero");
+const sectionHeroEl = document.querySelector('.section-hero');
 
 const obs = new IntersectionObserver(
   function (entries) {
@@ -58,18 +53,18 @@ const obs = new IntersectionObserver(
     console.log(ent);
 
     if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
+      document.body.classList.add('sticky');
     }
 
     if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
+      document.body.classList.remove('sticky');
     }
   },
   {
     // In the viewport
     root: null,
     threshold: 0,
-    rootMargin: "-80px",
+    rootMargin: '-80px',
   }
 );
 obs.observe(sectionHeroEl);
